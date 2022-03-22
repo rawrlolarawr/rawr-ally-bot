657f43749d9ede48a263725e8f99d61450da058c
Lola Jane Chaison <lolachaison@gmail.com>
2022-03-22 14:14:56 -0400

    initial commit

---
+++ src/bot.ts
@@ -0,0 +1,18 @@
+import { Client, ClientOptions } from 'discord.js';
+import 'dotenv/config'
+
+import ready from './listeners/ready'
+import interactionCreate from './listeners/interactionCreate'
+
+const token = process.env.DISCORD_TOKEN
+
+console.log("Ally is starting!");
+
+const client = new Client({
+    intents: []
+});
+
+ready(client)
+interactionCreate(client)
+
+client.login(token)
---
+++ src/listeners/interactionCreate.ts
@@ -0,0 +1,13 @@
+import { BaseCommandInteraction, Client, Interaction } from "discord.js";
+
+export default (client: Client): void => {
+    client.on("interactionCreate", async (interaction: Interaction) => {
+        if (interaction.isCommand() || interaction.isContextMenu()) {
+            await handleSlashCommand(client, interaction);
+        }
+    });
+};
+
+const handleSlashCommand = async (client: Client, interaction: BaseCommandInteraction): Promise<void> => {
+    // handle slash command here
+};
